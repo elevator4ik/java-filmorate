7ac63d07afb6d85a982a6c42f5c65b8d1d8227fe
Alexandr <alexandr.kocmo@yandex.ru>
2023-07-24 00:41:05 +0300

    codestyle3

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmByLikeComparator.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmByLikeComparator.java
@@ -6,7 +6,7 @@ public class FilmByLikeComparator implements Comparator<Film> {
     @Override
     public int compare(Film f1, Film f2) {
         int i = Integer.compare(f1.getLikes(), f2.getLikes());
-        if (i == 0) {//если результат сравнения равен — запись в сет не происходит,значит сортируем по id
+        if (i == 0) { //если результат сравнения равен — запись в сет не происходит,значит сортируем по id
             return Integer.compare(f1.getId(), f2.getId());
         } else {
             return -i;
